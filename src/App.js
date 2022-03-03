4a760219f191aea27598cbfbf2a7da2dbecb64c2
shivam <shivambitid007@gmail.com>
2022-03-03 19:54:53 +0530

    Homepage is split

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
-import HomePage from './homepage.components';
+import HomePage from './pages/hompage/homepage.components';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<HomePage/>}/>
+        </Routes>    
+      </div>
+    </Router>
   );
 }
 
